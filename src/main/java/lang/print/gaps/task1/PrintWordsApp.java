ba306b318d270bc90d6465c79ac8eabc27cf38ae
Admin <budda4613@gmail.com>
2022-09-07 19:18:48 +0500

    module #3

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -1,7 +1,8 @@
 package lang.print.gaps.task1;
 
 public class PrintWordsApp {
-    public static void main(String[] args) {
-        //write your code here
+    public static void main(String[] args)
+    {
+        System.out.println("java"+"\n"+"is"+"\n"+"smth");//write your code here
     }
 }
